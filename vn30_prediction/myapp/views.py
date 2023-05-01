751a4abfbc0f69246a6d798e2d8bfe772862c18f
huyvoUIT_spiraledge <vqh.official@gmail.com>
2023-05-01 14:49:02 +0700

    fix

--- vn30_prediction/myapp/views.py
+++ vn30_prediction/myapp/views.py
@@ -6,27 +6,37 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-url = 'https://raw.githubusercontent.com/huy164/datasets/master/VN30_price.csv'
-df = pd.read_csv(url)
+# url = 'https://raw.githubusercontent.com/huy164/datasets/master/VN30_price.csv'
+# df = pd.read_csv(url)
 
 
-data = df['VN30'].values.reshape(-1, 1)
-scaler = MinMaxScaler(feature_range=(0, 1))
-data = scaler.fit_transform(data)
+# data = df['VN30'].values.reshape(-1, 1)
+# scaler = MinMaxScaler(feature_range=(0, 1))
+# data = scaler.fit_transform(data)
 
 
-model = load_model('/home/huy/Documents/Desktop/thesis/vn30_prediction/myapp/lstm_model.h5')
+# model = load_model('/home/huy/Documents/Desktop/thesis/vn30_prediction/myapp/lstm_model.h5')
 
 
 @api_view(['GET'])
 def predict_vn30(request):
+    url = 'https://raw.githubusercontent.com/huy164/datasets/master/VN30_price.csv'
+    df = pd.read_csv(url)
+
+    # Preprocess the data
+    data = df['VN30'].values.reshape(-1, 1)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data = scaler.fit_transform(data)
+
     seq_length = 5
     last_sequence = data[-seq_length:]
     last_sequence = np.reshape(last_sequence, (1, seq_length, 1))
-    
+
+
+    model = load_model('lstm_model.h5')
 
     next_day_prediction = model.predict(last_sequence)
     next_day_prediction = scaler.inverse_transform(next_day_prediction)
-    
+
 
     return Response({'vn30_prediction': next_day_prediction[0, 0]})
