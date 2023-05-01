97ebc156ca974a0014f52904506faf3364f71217
huyvoUIT_spiraledge <vqh.official@gmail.com>
2023-05-01 13:52:23 +0700

    fix

--- vn30_prediction/vn30_prediction/urls.py
+++ vn30_prediction/vn30_prediction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import predict_vn30
+from myapp.views import predict_vn30
 
 urlpatterns = [
     path('predict_vn30/', predict_vn30, name='predict_vn30'),
