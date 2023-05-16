4cfdb666a80e741b86c905f0617e9e9a0f79adaf
huyvoUIT_spiraledge <vqh.official@gmail.com>
2023-05-16 08:11:43 +0700

    add prediction api

--- vn30_prediction/myapp/views.py
+++ vn30_prediction/myapp/views.py
@@ -1,6 +1,10 @@
 import csv
-import urllib.request
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+import numpy as np
+from tensorflow.keras.models import load_model
+import os
+
 
 def get_vn30_history(request):
     data = []
@@ -16,3 +20,47 @@ def get_vn30_history(request):
             row['Change'] = float(row['Change'].replace('%', ''))
             data.append(row)
     return JsonResponse(data, safe=False)
+
+@require_GET
+def predict_view(request):
+    # Get parameters from the request
+    predict_range = int(request.GET.get('predict_range', 30))
+    algorithm = request.GET.get('algorithm', 'lstm')
+
+    # Load the appropriate model based on the algorithm parameter
+    if algorithm == 'lstm':
+        model = load_model(os.path.join(os.path.dirname(__file__), 'lstm.h5'))
+    else:
+        # Handle other algorithm cases or raise an error
+        return JsonResponse({'error': 'Unsupported algorithm'})
+
+    dataset = read_csv('https://raw.githubusercontent.com/huy164/datasets/master/VN30_price.csv', header=0, index_col=0)
+    # get last n_days_lag days of data from dataset
+    last_n_days = dataset.values[-n_days_lag:, :]
+    # scale data using same scaler fit on training data
+    last_n_days_scaled = scaler.transform(last_n_days)
+    # reshape data into 3D shape for input into LSTM model
+    input_data = last_n_days_scaled.reshape((1, n_days_lag, n_features))
+    # generate predictions for next 30 days
+    predictions = []
+    for i in range(predict_range):
+        # make prediction for next day
+        yhat = model.predict(input_data)
+        # add prediction to list of predictions
+        predictions.append(yhat[0])
+        # update input_data with new prediction
+        input_data = np.roll(input_data, -1)
+        input_data[0, -1, :] = yhat
+    # inverse transform predictions to obtain final predicted values
+    predictions = np.array(predictions)
+    # create dummy array with same shape as other features in dataset
+    dummy_values = np.zeros((predictions.shape[0], n_features - 1))
+    # concatenate predictions with dummy values
+    predictions_with_dummy = np.concatenate((dummy_values, predictions), axis=1)
+    # inverse transform predictions to obtain final predicted values
+    inv_yhat = scaler.inverse_transform(predictions_with_dummy)
+    # extract predicted values from last column
+    inv_yhat = inv_yhat[:, -1]
+
+    # Return the predictions as a JSON response
+    return JsonResponse({'predictions': inv_yhat.tolist()})
--- vn30_prediction/vn30_prediction/urls.py
+++ vn30_prediction/vn30_prediction/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from myapp.views import get_vn30_history
+from myapp.views import predict_view
 
 urlpatterns = [
     path('get-vn30-history/', get_vn30_history, name='get_vn30_history'),
+    path('api/predictions/', predict_view, name='predictions'),
+
 ]
